26826e3b1fb458bf19f94acc43f801503ca4434b
Jhon Sanchez <jhonhst2@gmail.com>
2022-07-24 13:41:43 -0500

    added google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
 import '@styles/globals.scss'
 import type { AppProps } from 'next/app'
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const NEXT_PUBLIC_GOOGLE_ANALYTICS = 'G-TC5MEN8H66'
+  return <>
+
+    <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+
+    <Script strategy="lazyOnload">
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+    `}
+    </Script>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
